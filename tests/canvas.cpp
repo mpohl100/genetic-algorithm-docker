ab17480b8448002c750563e10324ac9e3681fa76
Michael Pohl <mpohl100@googlemail.com>
2023-09-12 20:35:02 +0200

    implemented draw_line and draw_rectangle

--- tests/canvas.cpp
+++ tests/canvas.cpp
@@ -9,7 +9,7 @@ TEST_CASE("Canvas", "[canvas]"){
         auto canvas = path::Canvas2D(5,5);
         canvas.draw_rectangle(path::Point(1,1), path::Point(3,3));
         const auto canvas_pixels = canvas.getPixels();
-        CHECK(canvas_pixels == std::string(".....\n.XXX.\n.X.X.\n.XXX.\n....."));
+        CHECK(canvas_pixels == std::string(".....\n.XXX.\n.X.X.\n.XXX.\n.....\n"));
     }
 }
 

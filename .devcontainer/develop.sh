a0f48e4d4fe3d36c1dd98cd4fbd50b0902ead940
Michael Pohl <mpohl100@googlemail.com>
2023-10-18 17:55:49 +0200

    new devcontainer config

--- .devcontainer/develop.sh
+++ .devcontainer/develop.sh
@@ -4,4 +4,4 @@
 docker build -t develop-image $(pwd)/.devcontainer/.
 
 # Run the Docker container interactively with the /project directory mounted
-docker run -it -v $(pwd):/workspace develop-image
+docker run -it -v $(pwd):/workspace -e EXECUTE_BIN_BASH=1 develop-image

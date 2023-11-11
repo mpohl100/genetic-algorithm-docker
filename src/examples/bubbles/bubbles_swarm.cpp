9f9465a4ba736ce355c8986d1133817a5c291ba1
Michael Pohl <mpohl100@googlemail.com>
2023-11-11 07:35:57 +0000

    introduced already optimized data structure in bubbles swarm

--- src/examples/bubbles/bubbles_swarm.cpp
+++ src/examples/bubbles/bubbles_swarm.cpp
@@ -2,14 +2,14 @@
 
 namespace bubbles {
 
-BubblesSwarm bubbles_algorithm([[maybe_unused]] const Canvas2D& canvas, [[maybe_unused]] const Point& point)
-{
-    return BubblesSwarm();
+BubblesSwarm bubbles_algorithm([[maybe_unused]] const Canvas2D &canvas,
+                               [[maybe_unused]] const Point &point) {
+  return BubblesSwarm{};
 }
 
-double BubblesSwarm::area() const
-{
-    return 0.0;
-}
+BubblesSwarm::BubblesSwarm(const AlreadyOptimized &already_optimized)
+    : _circle{}, _already_optimized{already_optimized} {}
 
-}
+double BubblesSwarm::area() const { return 0.0; }
+
+} // namespace bubbles
--- src/examples/bubbles/bubbles_swarm.h
+++ src/examples/bubbles/bubbles_swarm.h
@@ -2,15 +2,37 @@
 
 #include "evol.h"
 
-
+#include <optional>
+#include <vector>
 
 namespace bubbles {
 
 class BubblesSwarm {
 public:
-    double area() const;
+  struct AlreadyOptimized {
+    std::vector<Circle> circles;
+    struct SourceCirle {
+      size_t index = 0;
+      int quadrant = 0;
+      friend constexpr auto operator<=>(const SourceCirle &,
+                                        const SourceCirle &) = default;
+    };
+    std::optional<SourceCirle> source_circle = std::nullopt;
+  };
+
+  BubblesSwarm() = default;
+  BubblesSwarm(const BubblesSwarm &) = default;
+  BubblesSwarm &operator=(const BubblesSwarm &) = default;
+  BubblesSwarm(BubblesSwarm &&) = default;
+  BubblesSwarm &operator=(BubblesSwarm &&) = default;
+  BubblesSwarm(const AlreadyOptimized &already_optimized);
+  double area() const;
+
+private:
+  Circle _circle;
+  AlreadyOptimized _already_optimized;
 };
 
-BubblesSwarm bubbles_algorithm(const Canvas2D& canvas, const Point& point);
+BubblesSwarm bubbles_algorithm(const Canvas2D &canvas, const Point &point);
 
-} 
+} // namespace bubbles

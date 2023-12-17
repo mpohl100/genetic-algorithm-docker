dcefdc3bdcbdca6cc4ba984b9998290010841d94
Michael Pohl <mpohl100@googlemail.com>
2023-12-17 16:20:15 +0000

    imported smoothing function

--- src/examples/bubbles/detection/Detection.cpp
+++ src/examples/bubbles/detection/Detection.cpp
@@ -17,4 +17,70 @@ cv::Mat detect_directions(cv::Mat const &bgrImg) {
 cv::Mat detect_angles(cv::Mat const &bgrImg) {
   return detail::detect_edges<detail::DetectionType::Angle>(bgrImg);
 }
+
+cv::Mat smooth_angles(cv::Mat const &angles, int rings, bool onlyRecordAngles) {
+  cv::Mat result = angles.clone();
+  std::vector<const cv::Vec3b *> rows;
+  cv::Vec3b *resultRow = nullptr;
+  for (size_t i = 0; i < 2 * rings + 1; ++i)
+    rows.push_back(nullptr);
+  for (int i = rings; i < angles.rows - rings; ++i) {
+    resultRow = result.ptr<cv::Vec3b>(i);
+    size_t index = 0;
+    for (size_t j = i - rings; j < i + rings + 1; ++j)
+      rows[index++] = angles.ptr<cv::Vec3b>(j);
+    for (int j = rings; j < angles.cols - rings; ++j) {
+      double sumAngle = 0;
+      double sumLen = 0;
+
+      for (int k = 0; k < int(rows.size()); ++k)
+        for (int l = j - rings; l < j + rings + 1; ++l) {
+
+          double len = double(rows[k][l][0]);
+          if (len > 0) {
+            sumLen += len;
+            int angle = rows[k][l][1];
+            if (angle > 0)
+              sumAngle += len * angle;
+            else
+              sumAngle -= len * rows[k][l][2];
+          }
+        }
+
+      int nb = 2 * rings + 1;
+      nb *= nb; // squared
+      if (sumLen / nb < 20) {
+        resultRow[j][0] = 256;
+        resultRow[j][1] = 256;
+        resultRow[j][2] = 256;
+      } else {
+        double angle = sumAngle / sumLen;
+        double len = sumLen / nb;
+        resultRow[j][0] = int(len);
+        if (angle > 0) {
+          if (!onlyRecordAngles) {
+            resultRow[j][1] = int(angle);
+            resultRow[j][2] = 0;
+          } else {
+            resultRow[j][0] = int(angle * 256.0 / 180.0);
+            resultRow[j][1] = 0;
+            resultRow[j][2] = 0;
+          }
+        } else {
+          if (!onlyRecordAngles) {
+            resultRow[j][1] = 0;
+            resultRow[j][2] = int(-angle);
+          } else {
+            resultRow[j][0] = 0;
+            int angleInt = int(-angle * 256.0 / 180.0);
+            resultRow[j][1] = angleInt;
+            resultRow[j][2] = 0;
+          }
+        }
+      }
+    }
+  }
+  return result;
+}
+
 } // namespace od
--- src/examples/bubbles/detection/Detection.h
+++ src/examples/bubbles/detection/Detection.h
@@ -7,4 +7,7 @@ namespace od {
 cv::Mat detect_directions(cv::Mat const &bgrImg);
 // cv::Mat detect_edges_gray(cv::Mat const& bgrImg);
 cv::Mat detect_angles(cv::Mat const &bgrImg);
+
+cv::Mat smooth_angles(cv::Mat const &angles, int rings, bool onlyRecordAngles);
+
 } // namespace od

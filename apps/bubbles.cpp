e342150c1ff5ff9383a78fa908a52f3726e8ffe5
Michael Pohl <mpohl100@googlemail.com>
2023-12-25 20:02:54 +0000

    add canvas size as parameter to bubble executable

--- apps/bubbles.cpp
+++ apps/bubbles.cpp
@@ -15,16 +15,24 @@ int main(int argc, char **argv) {
   int tl_y = 20;
   int rectangle_x = 50;
   int rectangle_y = 50;
+  int canvas_width = 100;
+  int canvas_height = 100;
   bool help = false;
   auto cli =
       Opt(start_x, "start_x")["-x"]["--start-x"]("The start x coordinate") |
       Opt(start_y, "start_y")["-y"]["--start-y"]("The start y coordinate") |
       Opt(tl_x, "tl_x")["-t"]["--tl-x"]("The top left x coordinate") |
       Opt(tl_y, "tl_y")["-u"]["--tl-y"]("The top left y coordinate") |
-      Opt(rectangle_x, "rectangle_x")["-r"]["--rectangle-x"](
-          "The rectangle x coordinate") |
+      Opt(rectangle_x,
+          "rectangle_x")["-r"]["--rectangle-x"]("The rectangle x coordinate") |
       Opt(rectangle_y, "rectangle_y")["-s"]["--rectangle-y"]("The rectangle y "
-                                                              "coordinate") |
+                                                             "coordinate") |
+      Opt(canvas_width, "canvas_width")["-w"]["--canvas-width"]("The width of "
+                                                                "the canvas") |
+      Opt(canvas_height, "canvas_height")["-h"]["--canvas-height"]("The "
+                                                                   "height of "
+                                                                   "the "
+                                                                   "canvas") |
       Help(help);
 
   auto result = cli.parse(Args(argc, argv));
@@ -37,14 +45,15 @@ int main(int argc, char **argv) {
     exit(0);
   }
 
-  auto canvas = bubbles::Canvas2D(100, 100);
+  auto canvas = bubbles::Canvas2D(canvas_width, canvas_height);
   auto tried_circles = tiles::CircleTiles(canvas.width(), canvas.height(), 10);
   const auto rectangle =
-      math2d::Rectangle{math2d::Point(tl_x, tl_y), math2d::Point(tl_x + rectangle_x, tl_y + rectangle_y)};
+      math2d::Rectangle{math2d::Point(tl_x, tl_y),
+                        math2d::Point(tl_x + rectangle_x, tl_y + rectangle_y)};
   canvas.draw_rectangle(rectangle);
-  const auto already_optimized =
-      bubbles::bubbles_algorithm_slow(canvas, math2d::Point(start_x, start_y), tried_circles);
-  for(const auto& circle : already_optimized.circles()) {
+  const auto already_optimized = bubbles::bubbles_algorithm_slow(
+      canvas, math2d::Point(start_x, start_y), tried_circles);
+  for (const auto &circle : already_optimized.circles()) {
     canvas.draw_circle(circle);
   }
   const auto canvas_pixels = canvas.getPixels();

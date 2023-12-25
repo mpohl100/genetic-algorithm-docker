0f43ff96a5f3cc5e39f13e228c037b7b88bddc96
Michael Pohl <mpohl100@googlemail.com>
2023-12-25 07:20:42 +0000

    establishing shot now can do a specified number of rectangles

--- apps/establishing_shot.cpp
+++ apps/establishing_shot.cpp
@@ -13,14 +13,20 @@ int main(int argc, char **argv) {
   int tl_y = 20;
   int rectangle_x = 50;
   int rectangle_y = 50;
+  size_t N_rectangles_x = 2;
+  size_t N_rectangles_y = 2;
   bool help = false;
   auto cli =
       Opt(tl_x, "tl_x")["-t"]["--tl-x"]("The top left x coordinate") |
       Opt(tl_y, "tl_y")["-u"]["--tl-y"]("The top left y coordinate") |
-      Opt(rectangle_x, "rectangle_x")["-r"]["--rectangle-x"](
-          "The rectangle x coordinate") |
+      Opt(rectangle_x,
+          "rectangle_x")["-r"]["--rectangle-x"]("The rectangle x coordinate") |
       Opt(rectangle_y, "rectangle_y")["-s"]["--rectangle-y"]("The rectangle y "
-                                                              "coordinate") |
+                                                             "coordinate") |
+      Opt(N_rectangles_x,
+          "N_x")["-x"]["--N-x"]("The number of rectangles in x direction") |
+      Opt(N_rectangles_y,
+          "N_y")["-y"]["--N-y"]("The number of rectangles in y direction") |
       Help(help);
 
   auto result = cli.parse(Args(argc, argv));
@@ -34,16 +40,22 @@ int main(int argc, char **argv) {
   }
 
   auto canvas = bubbles::Canvas2D(100, 100);
-  const auto rectangle =
-      math2d::Rectangle{math2d::Point(tl_x, tl_y), math2d::Point(tl_x + rectangle_x, tl_y + rectangle_y)};
-  canvas.draw_rectangle(rectangle);
+  for (size_t i = 0; i < N_rectangles_x; ++i) {
+    for (size_t j = 0; j < N_rectangles_y; ++j) {
+      const auto rectangle = math2d::Rectangle{
+          math2d::Point(tl_x + i * rectangle_x, tl_y + j * rectangle_y),
+          math2d::Point(tl_x + (i + 1) * rectangle_x,
+                        tl_y + (j + 1) * rectangle_y)};
+      canvas.draw_rectangle(rectangle);
+    }
+  }
   const auto all_rectangles = bubbles::establishing_shot(canvas);
-  for(const auto& rectangle : all_rectangles.rectangles) {
+  for (const auto &rectangle : all_rectangles.rectangles) {
     canvas.draw_rectangle(rectangle.to_math2d_rectangle());
   }
   const auto canvas_pixels = canvas.getPixels();
+  std::cout << canvas_pixels << '\n';
   std::cout << "All rectangles: " << all_rectangles.rectangles.size() << "\n";
   std::cout << '\n';
-  std::cout << canvas_pixels << '\n';
   return 0;
 }

f711cb869c58f76c9cbbc1bec8e2fdb736820749
Michael Pohl <mpohl100@googlemail.com>
2024-01-04 22:22:18 +0000

    implemented test case for process_frame

--- tests/webcam.cpp
+++ tests/webcam.cpp
@@ -5,6 +5,8 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
+#include <taskflow/taskflow.hpp>
+
 #include <iostream>
 
 namespace {
@@ -12,14 +14,33 @@ namespace {
 TEST_CASE("Webcam", "[webcam]") {
 
   SECTION("WebcamProcessFrame") {
+    tf::Executor executor(4);
+    int rings = 1;
+    int gradient_threshold = 20;
     const auto path = "../video/BillardTakeoff.mp4";
+
     auto cap = cv::VideoCapture{path};
     if (!cap.isOpened()) {
       std::cout << "!!! Input video could not be opened" << std::endl;
       throw std::runtime_error("Cannot open input video");
     }
     CHECK(cap.isOpened());
-    CHECK(false);
+
+    cv::Mat imgOriginal;
+    int retflag = -1;
+    webcam::read_image_data(cap, imgOriginal, retflag);
+
+    CHECK(retflag != 2);
+    if (retflag == 2) {
+      return;
+    }
+
+    const auto rectangle =
+        bubbles::Rectangle{0, 0, imgOriginal.cols, imgOriginal.rows};
+    const auto frame_data = webcam::process_frame(
+        imgOriginal, rectangle, executor, rings, gradient_threshold);
+
+    CHECK(frame_data.all_rectangles.rectangles.size() == 3335);
   }
 }
 

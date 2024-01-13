0e940a39ee1e95d11790da38c813335a491695b9
Michael Pohl <mpohl100@googlemail.com>
2024-01-13 06:25:16 +0100

    qualify rectangle

--- src/examples/bubbles/detection/Detection.h
+++ src/examples/bubbles/detection/Detection.h
@@ -6,10 +6,10 @@
 
 namespace od {
 
-void detect_directions(cv::Mat &ret, cv::Mat const &bgrImg, const Rectangle& rectangle);
+void detect_directions(cv::Mat &ret, cv::Mat const &bgrImg, const od::Rectangle& rectangle);
 // cv::Mat detect_edges_gray(cv::Mat const& bgrImg);
-void detect_angles(cv::Mat &ret, cv::Mat const &bgrImg, const Rectangle& rectangle);
+void detect_angles(cv::Mat &ret, cv::Mat const &bgrImg, const od::Rectangle& rectangle);
 
-void smooth_angles(cv::Mat &result, cv::Mat const &angles, int rings, bool onlyRecordAngles, int threshold, const Rectangle& rectangle);
+void smooth_angles(cv::Mat &result, cv::Mat const &angles, int rings, bool onlyRecordAngles, int threshold, const od::Rectangle& rectangle);
 
 } // namespace od
